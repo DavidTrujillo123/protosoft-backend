7c9d03fa0c37cffa9f7a718cade565a670c5d1ad
clousck <victorecheverriasoto@gmail.com>
2023-07-10 12:12:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -44,5 +44,5 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
+    next();
+});
