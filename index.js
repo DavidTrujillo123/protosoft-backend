aa4ce0dfafe387885c17540e32bdd30b9d8f7ebf
clousck <victorecheverriasoto@gmail.com>
2023-07-10 22:43:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,13 +14,7 @@ const getUser = async (id) =>
 
     return data.rows
 }
-const postUser = async (body) =>
-{
-    let user = Object.values(body);
-    var query = `INSERT INTO users (useremail, userpassword) VALUES($1, $2)`;
-    await pool.query(query, user);
-}
-const getProducts = async (id) =>
+const getProduct = async (id) =>
 {
     if(id == null)
         var data = (await db.query('SELECT * FROM productos'))
@@ -30,14 +24,18 @@ const getProducts = async (id) =>
     return data.rows
 }
 
+const postUser = async (body) =>
+{
+    let user = Object.values(body);
+    var query = `INSERT INTO users (useremail, userpassword) VALUES($1, $2)`;
+    await pool.query(query, user);
+}
+
 app.get('/users', async (req, res)=>{res.send(await getUser())})
-app.get('/products', async (req, res)=>{res.send(await getProducts())})
-app.get('/', async (req, res)=>{res.send(davidtrujillo123.github.io/protosoft_v1/html/home.html)})
+app.get('/products', async (req, res)=>{res.send(await getProduct())})
+app.get('/', async (req, res)=>{res.send('Home')})
 
 app.post('/user', (req, res)=>{postUser(req.body)})
-
-
-
 
 
 app.use((req, res, next) => {
